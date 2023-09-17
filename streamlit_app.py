e838a1c8f8e44ed311f5aa94f15ffbe19c6c148f
SnowSudip <145289292+SnowSudip@users.noreply.github.com>
2023-09-18 00:39:39 +0530

    Update streamlit_app.py
    Added images breakfast items
    changed header
    added new menu item

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 streamlit.title("My Parents new Healthy Diner")
-streamlit.header("Breakfast Menu")
+streamlit.header("Breakfast Favorites")
 
-streamlit.text("Omega3 and Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Eggs")
+streamlit.text("🥣Omega3 and Blueberry Oatmeal")
+streamlit.text("🥗Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔Hard-Boiled Free-Range Eggs")
+streamlit.text("🥑🍞Avocado Toast")
+
